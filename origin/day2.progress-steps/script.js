9ed40749c0b221cf475fb26fe24c9c6206d3ab5e
MarkTuwen <m18698577021@163.com>
2025-01-03 21:06:44 +0800

    day2 Progress Steps html+css+js

--- origin/day2.progress-steps/script.js
+++ origin/day2.progress-steps/script.js
@@ -1,39 +1,35 @@
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
+const progress = document.querySelector('#progress')
+const prev = document.querySelector('#prev')
+const next = document.querySelector('#next')
 const circles = document.querySelectorAll('.circle')
-const progress = document.getElementById("progress")
 
 let currentActive = 1
 
-prev.addEventListener('click', () => {
-  if (currentActive > 1) {
-    currentActive--
-  }
-  update()
-})
-
 next.addEventListener('click', () => {
-  if (currentActive < circles.length) {
-    currentActive++
+  currentActive++
+  if (currentActive > circles.length) {
+    currentActive = circles.length
   }
   update()
 })
 
-function update() {
-  // 更新active类
-  circles.forEach((circle, idx) => {
-    if (idx < currentActive) {
+prev.addEventListener('click', () => {
+  currentActive--
+  if (currentActive < 1) {
+    currentActive = 1
+  }
+  update()
+})
+
+const update = () => {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
       circle.classList.add('active')
     } else {
       circle.classList.remove('active')
     }
   })
-
-  // 更新进度条长度
-  const actives = document.querySelectorAll('.active')
-  progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
-
-  // 更新prev和next按钮状态
+  progress.style.width = ((currentActive - 1) / (circles.length - 1)) * 100 + '%'
   if (currentActive === 1) {
     prev.disabled = true
   } else if (currentActive === circles.length) {
