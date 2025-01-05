228abdb261b616b8be5d1d4ba1d65e894d24c934
MarkTuwen <m18698577021@163.com>
2025-01-05 18:56:05 +0800

    modify favicon name from favicon.ico to icon.ico to fix favicon not found issue cause seems head config isn not in effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./favicon.ico" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
